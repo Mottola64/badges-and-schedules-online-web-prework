7d864fde3f993a054c8858c2364e1c9fa8843f72
Michael Mottola <mottola64@gmail.com>
2019-02-25 02:12:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(names)
+  names.each do |name|
+    name = 
+    print "Hello, my name is #{name}"
+end
+end
