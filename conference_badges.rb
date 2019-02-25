9b584f92e847cac9c998b10dae0adf56295b86f9
Michael Mottola <mottola64@gmail.com>
2019-02-25 02:30:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,23 @@ end
 return badges
 end
 
-def assign_rooms(names)
+def assign_rooms(speakers)
+  room_assignments = []
+  range = speakers.length
+  for room_num in 1..range do
+    speaker = speakers[room_num-1]
+    room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{room_num}!")
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+  assignments = assign_rooms(attendees)
+  assignments.each do |room|
+    puts room
+  end
+end
